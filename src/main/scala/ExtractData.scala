06658fb0d6e67bf16422fdf45fabb3e489e840e0
Alan Lewis <alan.lewis@objectaction.com>
2017-05-07 14:02:30 +0100

    Fix code to use UUID instead of String. All tests passing.

--- src/main/scala/ExtractData.scala
+++ src/main/scala/ExtractData.scala
@@ -1,5 +1,5 @@
 import java.time.ZonedDateTime
-
+import java.util.UUID
 import scala.io.Source._
 import scala.language.postfixOps
 import scala.math.BigDecimal.RoundingMode
@@ -15,7 +15,7 @@ import org.json4s.native.JsonMethods._
  */
 object ExtractData {
 	case class RawRecord(sensor_id: String, timestamp: String, consumption_Wh: Int)
-	case class Record(sensor_id: String, timestamp: ZonedDateTime, consumption_Wh: Int)
+	case class Record(sensor_id: UUID, timestamp: ZonedDateTime, consumption_Wh: Int)
 
 	// for a production system, these would be either runtime parameters or
 	// perhaps read from a stream.
@@ -23,32 +23,27 @@ object ExtractData {
 	private val jsonFileName = "src/main/resources/consumption_data.json"
 	// this 'caches' the data from the file for convenience, as it's reused
 	// for both json and csv formatted data.
-	private var sensorDataCollection : Seq[Record] = List()
+	private var sensorDataCollection: Seq[Record] = List()
 
-	// The sensor ID, (first field), is a UUID, but a string is used 
-	// for the purpose of the exercise. It should take little effort to 
-	// change the String to a java.util.UUID
 	private def readDataFromCSV: Seq[Record] = {
 		// drop 1st row from data file as it contains only column names and not values.
-		val data = fromFile(csvFileName).getLines.toList.map(x => x.split(" ")).drop(1)
+		val data = fromFile(csvFileName).getLines.toList.map(_.split(" ")).drop(1) 
 		for {
-			l <- data 
+			l <- data
 			v = l(0).split(",")
-		} yield Record(v(0), ZonedDateTime.parse(v(1)), v(2).toInt)
+		} yield Record(UUID.fromString(v(0)), ZonedDateTime.parse(v(1)), v(2).toInt)
 	}
 
 	implicit lazy val formats = org.json4s.DefaultFormats
+
 	// TODO:  create a formatter to convert String to ZonedDateTime. That would
 	// eliminate the need to convert between 2 case classes!
-
 	private def readDataFromJson: Seq[Record] = {
-		var json = parse(fromFile(jsonFileName).mkString)
-		// this can be fixed with implicit format function
-		val something = json.extract[List[RawRecord]] 
+		var json = parse(fromFile(jsonFileName).mkString).extract[List[RawRecord]]
 		for {
-			r <- something
+			r <- json
 			rec = r match {
-				case RawRecord(i,t,kw) => Record(i, ZonedDateTime.parse(t), kw)
+				case RawRecord(s,t,k) => Record(UUID.fromString(s), ZonedDateTime.parse(t), k)
 			}
 		} yield rec
 	}
@@ -63,16 +58,16 @@ object ExtractData {
 		sensorDataCollection
 	}
 
-	def sensorData(sensor: String): Seq[Record] = {
+	def sensorData(sensor: UUID): Seq[Record] = {
 		if(sensorDataCollection.isEmpty) sensorDataCollection = readDataFromCSV
-		sensorDataCollection filter { case Record(s,t,k) => s.equals(sensor) }
+		sensorDataCollection filter { case Record(s,t,k) => sensor.equals(s) }
 	}
 
-	def aggregateDailyConsumptionkWh(sensor: String): BigDecimal = 
+	def aggregateDailyConsumptionkWh(sensor: UUID): BigDecimal = 
 		BigDecimal((sensorData(sensor).map{ case Record(s,t,k) => k }.sum.toDouble)/1000).setScale(3, RoundingMode.UP)
 
-	def averageConsumptionForPeriod(duration: Range, sensor: String): BigDecimal = {
-		val bySensor = sensorData(sensor) filter { case Record(id, dt, kw) => duration.contains(dt.getHour()) }
-		BigDecimal(((bySensor map { case Record(s, d, k) => k } sum).toDouble / bySensor.length)/1000 ).setScale(6, RoundingMode.UP)
+	def averageConsumptionForPeriod(duration: Range, sensor: UUID): BigDecimal = {
+		val bySensor = sensorData(sensor) filter { case Record(s,t,k) => duration.contains(t.getHour()) }
+		BigDecimal(((bySensor map { case Record(s,t,k) => k } sum).toDouble / bySensor.length)/1000 ).setScale(6, RoundingMode.UP)
 	}
 }
